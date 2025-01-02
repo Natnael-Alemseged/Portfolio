ab45abf6be6b1a2619594b5b6b908211c3e3a406
Nati70-1 <-2>
2025-01-02 11:54:38 +0300

    new features to new branch

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import About from './sections/About.tsx'; // Assuming About is a .jsx file
 import Footer from './sections/Footer.jsx'; // Assuming Footer is a .jsx file
 import Navbar from './sections/Navbar.jsx'; // Assuming Navbar is a .jsx file
 import Contact from './sections/Contact.jsx'; // Assuming Contact is a .jsx file
-// import Clients from './sections/Clients.jsx'; // Assuming Clients is a .jsx file
+import Clients from './sections/Clients.jsx'; // Assuming Clients is a .jsx file
 import Projects from './sections/Projects.jsx';
 import WorkExperience from './sections/Experience';
 // import SelectedProjects from './sections/SelectedProjects.jsx';
@@ -15,7 +15,7 @@ const App: React.FC = () => {
             <Hero />
             <About />
             <Projects />
-            {/*<Clients />*/}
+            <Clients />
             {/*<SelectedProjects />*/}
             <WorkExperience />
             <Contact />
